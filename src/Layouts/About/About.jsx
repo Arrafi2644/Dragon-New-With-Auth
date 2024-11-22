126058d3f92338e55d1a9a7f17f2df1d366f8676
Arrafi2644 <raffian2644@gmail.com>
2024-11-22 09:10:22 +0600

    check

--- src/Layouts/About/About.jsx
+++ src/Layouts/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <div className='w-11/12 mx-auto'>
             <Header></Header>
             <Navbar></Navbar>
-            <h2 className='text-2xl font-semibold mt-8 text-center'>About page</h2>
+            <h2 className='text-2xl font-semibold mt-8 text-center'>About page this is </h2>
         </div>
     );
 };
